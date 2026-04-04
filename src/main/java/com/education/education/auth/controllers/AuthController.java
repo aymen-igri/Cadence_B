bdd1f99921f85970a56b6fce422833aed676127f
0XAymax <aymanderrouich3@gmail.com>
2026-04-04 23:55:31 +0100

    Relioving unecessary imports

--- src/main/java/com/education/education/auth/controllers/AuthController.java
+++ src/main/java/com/education/education/auth/controllers/AuthController.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.hibernate.boot.internal.Abstract;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
