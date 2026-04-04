959f7f5ac82c6f41a6e820fd4a452f6bb4638879
0XAymax <aymanderrouich3@gmail.com>
2026-04-04 23:55:52 +0100

    Changing SignupDTO Response structure

--- src/main/java/com/education/education/auth/deo/responses/SignUpDTOResponse.java
+++ src/main/java/com/education/education/auth/deo/responses/SignUpDTOResponse.java
@@ -1,14 +1,25 @@
 package com.education.education.auth.deo.responses;
 
-import com.education.education.user.role.dto.response.AddRoleToUserResponse;
-import com.education.education.user.user.dto.response.AddUserResponse;
-import com.education.education.user.user.enums.EGender;
-
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record SignUpDTOResponse(
-        AddUserResponse userData,
-        AddRoleToUserResponse userRole
+        Tokens tokens,
+        AuthUser user
 ) {
+    public record Tokens(
+            String accessToken,
+            String refreshToken
+    ) {}
+
+    public record AuthUser(
+            UUID id,
+            String firstName,
+            String lastName,
+            String username,
+            String email,
+            String phone,
+            String gender,
+            String role
+    ) {}
 }
+
