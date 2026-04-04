6114a0f648544fbd45b4ef2f2673bcf887ab8722
0XAymax <aymanderrouich3@gmail.com>
2026-04-04 23:56:23 +0100

    Removing unecessary imports

--- src/main/java/com/education/education/auth/mappers/AuthMapper.java
+++ src/main/java/com/education/education/auth/mappers/AuthMapper.java
@@ -1,7 +1,4 @@
 package com.education.education.auth.mappers;
-
-import com.education.education.auth.deo.responses.SignUpDTOResponse;
-import com.education.education.user.generalUser.entities.GeneralUser;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
