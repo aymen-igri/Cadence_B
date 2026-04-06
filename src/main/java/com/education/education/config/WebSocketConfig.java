0ed5bfab552f77e810ab0affb5c5f124d972621b
0XAymax <aymanderrouich3@gmail.com>
2026-04-06 17:41:31 +0100

    Update WebSocketConfig to enable message broker for queues and refine STOMP endpoint configuration

--- src/main/java/com/education/education/config/WebSocketConfig.java
+++ src/main/java/com/education/education/config/WebSocketConfig.java
@@ -70,14 +70,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic", "/queue");
         config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
     }
 }
