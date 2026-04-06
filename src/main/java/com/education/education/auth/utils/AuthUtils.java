366e51b8c6b75eab17e8adbf39d65e140df832e2
0XAymax <aymanderrouich3@gmail.com>
2026-04-06 16:31:21 +0100

    Implement JWT verification method in AuthUtils for token validation

--- src/main/java/com/education/education/auth/utils/AuthUtils.java
+++ src/main/java/com/education/education/auth/utils/AuthUtils.java
@@ -4,9 +4,20 @@ import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
 @Component
 @Getter
 public class AuthUtils {
     private String mySecret = Dotenv.load().get("MY_SECRET_KEY");
+
+    public DecodedJWT verifyDecodedJwtToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(mySecret);
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        return verifier.verify(token);
+    }
 }
 
