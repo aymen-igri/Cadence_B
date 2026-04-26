7e700f71c93bbaa3b7af0bf7941ef3956b937991
0XAymax <aymanderrouich3@gmail.com>
2026-04-26 17:14:00 +0100

    Add JWT secret configuration and Dockerfile for application deployment

--- src/main/java/com/education/education/auth/utils/AuthUtils.java
+++ src/main/java/com/education/education/auth/utils/AuthUtils.java
@@ -1,9 +1,8 @@
 package com.education.education.auth.utils;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -12,7 +11,9 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 @Component
 @Getter
 public class AuthUtils {
-    private String mySecret = Dotenv.load().get("MY_SECRET_KEY");
+
+    @Value("${jwt.secret}")
+    private String mySecret;
 
     public DecodedJWT verifyDecodedJwtToken(String token) {
         Algorithm algorithm = Algorithm.HMAC256(mySecret);
